0e0a63b0989962d320eb91ecc9575c5804c9dbdc
Alikhan-batyrgali <Alikhan.batyrgali2007@gmail.ru>
2024-03-20 10:51:25 +0500

    456

---
+++ home works 10.php
@@ -0,0 +1,33 @@
+<?php
+$a = 6;
+switch ($a) {
+ case 12:
+ case 1:
+ case 2:
+ echo "зима";
+ break;
+ 
+ case 3:
+ case 4:
+ case 5:
+ echo "весна";
+ break;
+ 
+ case 6:
+ case 7:
+ case 8:
+ echo "лето";
+ break;
+ 
+ case 9:
+ case 10:
+ case 11:
+ echo "осень";
+ break;
+ 
+ default:
+ echo "введите номер месяца(1-12)";
+ 
+}
+ 
+?>
---
+++ home works 7.php
@@ -0,0 +1,35 @@
+<?php
+// 1-понедельник
+// 2-вторник
+// 3-среда
+// 4-четверг
+// 5-пятница
+// 6-суббооа
+// 7-воскресение
+$a = 5;
+switch ($a) {
+ case 1:
+  echo "понедельник";
+  break;
+ case 2:
+  echo "вторник";
+  break;
+ case 3:
+  echo "среда";
+  break;
+ case 4:
+  echo "четверг";
+  break;
+ case 5:
+  echo "пятница";
+  break;
+ case 6:
+  echo "суббота";
+     break;
+ case 7:
+  echo "суббота";
+  break;
+ default:
+  echo "введите число 1-7";
+}
+?>
